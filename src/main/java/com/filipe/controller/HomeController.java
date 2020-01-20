0609a982f90a55008b9ff72dde5595dafbda093c
Filipe do Couto Castro <filipe.couto.10@gmail.com>
2020-01-20 18:14:10 -0300

    home

--- src/main/java/com/filipe/controller/HomeController.java
+++ src/main/java/com/filipe/controller/HomeController.java
@@ -2,6 +2,9 @@ package com.filipe.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class HomeController {
@@ -10,5 +13,10 @@ public class HomeController {
 	@GetMapping({ "/", "/home" })
 	public String home() {
 		return "home";
+	}
+	
+	@RequestMapping(value = "/upload")
+	public String handleFormUpload(@RequestParam("file") MultipartFile file) {
+		return "";
 	}
 }
